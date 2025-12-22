02d27bd7a42df6526b53c64f2513740e588fe38f
ntua-el21914 <el21914@mail.ntua.gr>
2025-12-22 16:43:31 +0200

    Island_Pass_Update

--- lib/features/camera/camera_screen.dart
+++ lib/features/camera/camera_screen.dart
@@ -20,10 +20,6 @@ class _CameraScreenState extends State<CameraScreen> {
   bool _isCameraInitialized = false;
   // bool _isUploading = false; // Δεν χρειάζεται πια εδώ
 
-  // --- ☁️ CLOUDINARY SETTINGS ☁️ ---
-  final String cloudName = "dkeski4ji";
-  final String uploadPreset = "CyclagoUserImages";
-
   @override
   void initState() {
     super.initState();
